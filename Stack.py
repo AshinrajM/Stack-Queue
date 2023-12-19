41d9b6a900339a2fcd8af29b6c5f433139c0740b
AshinrajM <ashinrajeevank@outlook.com>
2023-12-19 15:12:07 +0530

    stack

---
+++ Stack.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack:
+    def __init__(self, value):
+        NewNode = Node(value)
+        self.top = NewNode
+        self.height = 1
+
+    def display(self):
+        temp = self.top
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        NewNode = Node(value)
+        if self.top is None:
+            self.top = NewNode
+        else:
+            self.top.next = NewNode
+        self.height += 1
+
+    def pop(self):
+        if self.height is None:
+            return None
+        elif self.height == 1:
+            return None
+        else:
+            temp = self.top
+            self.top = temp.next
+            temp.next = None
+            self.height += 1
+            return temp
+
+    def peek(self):
+        if self.height:
+            return self.top
+        return None
+
+    def is_empty(self):
+        if self.top is None:
+            return True
+        return False
